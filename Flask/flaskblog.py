7376a0ca1e672081919bef4fd0ec6fae60b470fb
Saravanan <contactsaravanm@gmail.com>
2019-07-12 00:05:58 +0530

    template created

--- Flask/flaskblog.py
+++ Flask/flaskblog.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+		{
+			'author':'Saravanan',
+			'title':'Blog Post 1',
+			'content':'This is my new blog',
+			'date':'July'
+		}
+	]
 
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html',posts = posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Pag</h1>"
+    return "<h1>About Page</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
